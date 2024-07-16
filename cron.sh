327c8dee75fc3e88e71ba47a93f2b26eb8966ffe
yooml <ymlll0508@gmail.com>
2024-07-16 21:10:27 +0800

    fix: 🐛 rm cron.sh

--- cron.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /home/ubuntu/sora/snapshot/scripts
-node index.js 0 "../snapshots/vdot-$(date +"%Y-%m-%d").json"
-node vsdot.js 0 "../snapshots/salp-vsdot-$(date +"%Y-%m-%d").json"
-cd ..
-git add .
-git commit -m "Auto upload $(date +"%Y-%m-%d %T")"
-git push b main
